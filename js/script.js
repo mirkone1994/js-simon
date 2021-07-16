730876e1bd22c24073aa2db50ad0f1d608966b46
mirkone1994 <mirkochiappori@gmail.com>
2021-07-16 15:04:34 +0200

    corrections

--- js/script.js
+++ js/script.js
@@ -6,15 +6,13 @@ for (i=0; i<5; i++) {
     var randomNumbers = Math.floor(Math.random() * 100) + 1;
     numbers.push(randomNumbers);
 }
-alert(numbers);
+alert("Ricordati i seguenti numeri e inseriscili nella finestra di dialogo che ti comparirà tra 30 secondi, per ogni numero che ti ricorderai, il tuo punteggio incrementerà di 1: " + numbers);
 setTimeout(function(){
     for (i=0; i<5; i++) {
         askNumber = parseInt(prompt("Inserisci un numero da 1 a 100"));
         userNumbers.push(askNumber); 
         if (numbers[i] == userNumbers[i]) {
             result++;
-            console.log(numbers);
-            console.log(userNumbers);
         } else result + 0;
         console.log(result);
     }
